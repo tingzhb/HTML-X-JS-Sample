a7177ca0cc054be53c6130870d72752379300bcf
Benjamin Ting <tingzhb@gmail.com>
2024-09-19 14:10:35 +0200

    Sort Variable/Controller/View

--- HTMLPage/js/app.js
+++ HTMLPage/js/app.js
@@ -1,36 +1,32 @@
-// Document Elements
-// Looks for "text0". Assigns that to the variable called textField0.
-const textField0 = document.getElementById("text0");
+// VARIABLES
 
 // Buttons
-const increaseScoreButton = document.getElementById("button0");
+let button0 = document.getElementById("button0");
 
-// Internal Variables
-let score = 0;
+// Texts
+let playerRollText = document.getElementById("playerRollText");
 
-// Process (What is going to happen in what sequence?)
-increaseScoreButton.addEventListener("click", () => {
-  increaseScoreByOne();
-  updateScoreText();
-  checkScoreForSeven();
+// Data
+let playerRoll = 0;
+
+
+
+// PROCESSES
+button0.addEventListener("click", function() {
+  getRandomNumberOneToSixForPlayer();
+  showPlayerRollResult();
 });
 
 
-// Controllers
-function increaseScoreByOne() {
-  score++;
+
+
+// CONTROLLERS
+function getRandomNumberOneToSixForPlayer() {
+  playerRoll = Math.floor(Math.random() * 6) + 1;
 }
 
-function checkScoreForSeven() {
-  if (score >= 7) {
-    changeScoreTextColorToGreen();
-  }
-}
 
-// View
-function updateScoreText() {
-  textField0.innerHTML = "Your score is: " + score;
-}
-function changeScoreTextColorToGreen() {
-  textField0.style.color = "green";
+// VIEWS
+function showPlayerRollResult() {
+  playerRollText.innerHTML = playerRoll;
 }
